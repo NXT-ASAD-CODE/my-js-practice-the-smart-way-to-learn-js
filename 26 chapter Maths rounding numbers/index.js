d8ae4f23315ec528c00502ec50be7575426591b4
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-01-11 12:30:15 +0500

    rounding numbers

---
+++ 26 chapter Maths rounding numbers/index.js
@@ -0,0 +1,14 @@
+// Maths Rounding Numbers
+var scoreAvg = 7.5;
+
+// Rounding to the nearest integer
+var roundedScore = Math.round(scoreAvg);
+console.log("Rounded Score (Math.round):", roundedScore); // Output: 8
+// there are two operation ceil and floor
+
+// Rounding up to the next largest integer
+var ceilScore = Math.ceil(scoreAvg);
+console.log("Ceil Score (Math.ceil):", ceilScore); // Output: 8
+// Rounding down to the next smallest integer
+var floorScore = Math.floor(scoreAvg);
+console.log("Floor Score (Math.floor):", floorScore); // Output: 7
